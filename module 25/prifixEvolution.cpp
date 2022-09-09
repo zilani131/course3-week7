5c6a4e542e13b5a4c865f5e71fb6244c2aa1cdf9
zilani131 <zilani.cuet@gmail.com>
2022-09-09 21:57:09 +0600

    infix to prefix

--- module 25/prifixEvolution.cpp
+++ module 25/prifixEvolution.cpp
@@ -1,42 +1,123 @@
 #include<bits/stdc++.h>
 #include"MyStack.h"
 using namespace std;
-int prefixEvolution(string chk){
-    Stack<int> st;
-for(int i=chk.length()-1;i>=0;i--){
-    if(chk[i]>='0'&&chk[i]<='9'){
-            //we have to send integer so we converted the character into integer
-        st.push(chk[i]-'0');
+int precisionCalc(char s)
+{
+    if(s=='^')
+    {
+        return 3;
     }
-    else{
-            int a=st.pop();
-             int b=st.pop();
-        switch(chk[i]){
-    case '+':
-        st.push(a+b);
-        break;
-    case '-':
-        st.push(a-b);
-        break;
-    case '*':
-        st.push(a*b);
-        break;
-    case '/':
-        st.push(a/b);
-        break;
-    case '^':
-        st.push(pow(a,b));
-        break;
+    else if(s=='*'||s=='/')
+    {
+        return 2;
+    }
+    else if(s=='+'||s=='-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+string infixToPrifix(string chk)
+{
+    Stack<char> st;
+    reverse(chk.begin(),chk.end());
+    cout<<chk<<endl;
+    string result;
+    cout<<result<<endl;
+    for(int i=0; i<chk.length(); i++)
+    {
+        if(chk[i]>='0'&&chk[i]<='9')
+        {
+            result+=chk[i];
+        }
+        else if(chk[i]==')')
+        {
+
+            st.push(chk[i]);
+        }
+        else if(chk[i]=='(')
+        {
+            //st.empty() it is used to senati check if the last value is empty and to avoid it,we used it
+            while(!st.empty()&&st.Top()!=')')
+            {
+                result+= st.pop();
+            }
+            if(!st.empty())
+            {
+              //  cout<<result<<"before"<<endl;
+                st.pop();
+               // cout<<result<<"before"<<endl;
+            } //here we pop the ')' and clear the stack
+        }
+        else
+        {
+           // cout<<precisionCalc(st.Top())<<" "<<precisionCalc(chk[i])<<endl;
+            while(!st.empty()&&precisionCalc(st.Top())>=precisionCalc(chk[i]))
+            {
+                result+=st.pop();
+            }
+            st.push(chk[i]);
         }
     }
-
+    while(!st.empty())
+    {
+        result+=st.pop(); /////we pop until the last value
+    }
+   // cout<<result<<endl;
+    reverse(result.begin(),result.end());
+    return result;
 }
 
-return st.Top();
+
+int prefixEvolution(string chk)
+{
+    Stack<int> st;
+    for(int i=chk.length()-1; i>=0; i--)
+    {
+        if(chk[i]>='0'&&chk[i]<='9')
+        {
+            //we have to send integer so we converted the character into integer
+            st.push(chk[i]-'0');
+        }
+        else
+        {
+            int a=st.pop();
+            int b=st.pop();
+            cout<<a<<" "<<b<<endl;
+            switch(chk[i])
+            {
+            case '+':
+                st.push(a+b);
+                break;
+            case '-':
+                st.push(a-b);
+                break;
+            case '*':
+                st.push(a*b);
+                break;
+            case '/':
+                st.push(a/b);
+                break;
+            case '^':
+                st.push(pow(a,b));
+                break;
+            }
+        }
+
+    }
+
+    return st.Top();
 
 }
-int main(){
-cout<<prefixEvolution("+*423")<<endl;
-cout<<prefixEvolution("-+7*45+20");
+int main()
+{
+    string chk="(7+(4*5))-(2+0)";
+    cout<<infixToPrifix(chk)<<endl;
+
+    cout<<prefixEvolution(infixToPrifix(chk))<<endl;
+   // cout<<prefixEvolution("-+7*45+20");
 
 }
