526cbe1f5a198922c19cfe3b6cdc8240f56f23df
zilani131 <zilani.cuet@gmail.com>
2022-09-09 23:01:41 +0600

    post fix evolution done

--- module 25/prifixEvolution.cpp
+++ module 25/prifixEvolution.cpp
@@ -47,14 +47,14 @@ string infixToPrifix(string chk)
             }
             if(!st.empty())
             {
-              //  cout<<result<<"before"<<endl;
+                //  cout<<result<<"before"<<endl;
                 st.pop();
-               // cout<<result<<"before"<<endl;
+                // cout<<result<<"before"<<endl;
             } //here we pop the ')' and clear the stack
         }
         else
         {
-           // cout<<precisionCalc(st.Top())<<" "<<precisionCalc(chk[i])<<endl;
+            // cout<<precisionCalc(st.Top())<<" "<<precisionCalc(chk[i])<<endl;
             while(!st.empty()&&precisionCalc(st.Top())>=precisionCalc(chk[i]))
             {
                 result+=st.pop();
@@ -66,11 +66,42 @@ string infixToPrifix(string chk)
     {
         result+=st.pop(); /////we pop until the last value
     }
-   // cout<<result<<endl;
+    // cout<<result<<endl;
     reverse(result.begin(),result.end());
     return result;
 }
-
+string infixTopostFix(string chk)
+{
+    Stack<char> st;
+    string result;
+    for(int i=0; i<chk.length(); i++)
+    {
+        if(chk[i]>='0'&&chk[i]<='9')
+        {
+            result+=chk[i];
+        }
+        else if(chk[i]=='(')
+        {
+            st.push(chk[i]);
+        }
+        else if(chk[i]==')'){
+            while(!st.empty()&&st.Top()!='('){
+                    result+=st.pop();
+                  }
+            if(!st.empty())st.pop();
+        }
+        else{
+            while(!st.empty()&&precisionCalc(st.Top())>=precisionCalc(chk[i])){
+                result+=st.pop();
+            }
+            st.push(chk[i]);
+        }
+    }
+   while(!st.empty()){
+    result+=st.pop();
+   }
+   return result;
+}
 
 int prefixEvolution(string chk)
 {
@@ -112,12 +143,49 @@ int prefixEvolution(string chk)
     return st.Top();
 
 }
+int postfixEvolution(string chk)
+{
+    Stack <int> st;
+    for(int i=0; i<chk.length(); i++)
+    {
+        if(chk[i]>='0'&&chk[i]<='9')
+        {
+            st.push(chk[i]-'0');
+        }
+        else
+        {
+            int b=st.pop();
+            int a=st.pop();//must be care full in '-' and '/'
+            cout<<a<<" "<<b<<endl;
+            switch(chk[i])
+            {
+            case '+':
+                st.push(a+b);
+                break;
+            case '-':
+                st.push(a-b);
+                break;
+            case '*':
+                st.push(a*b);
+                break;
+            case '/':
+                st.push(a/b);
+                break;
+            case '^':
+                st.push(pow(a,b));
+                break;
+            }
+        }
+    }
+    return st.Top();
+}
 int main()
 {
     string chk="(7+(4*5))-(2+0)";
-    cout<<infixToPrifix(chk)<<endl;
+    cout<<infixTopostFix(chk)<<endl;
 
     cout<<prefixEvolution(infixToPrifix(chk))<<endl;
-   // cout<<prefixEvolution("-+7*45+20");
+    // cout<<prefixEvolution("-+7*45+20");
+    cout<<postfixEvolution(infixTopostFix(chk));
 
 }
